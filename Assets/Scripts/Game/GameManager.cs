544f46c846c0b7eb6093e982928bda1952b13c33
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-08-30 15:38:00 -0300

    Add game

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using Pathfinder;
+using UnityEngine;
+using Utils;
+
+namespace Game
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private int mapWidth;
+        [SerializeField] private int mapHeight;
+        [SerializeField] private GraphView graphView;
+        private Vector2IntGraph<Node<Vec2Int>> graph;
+        private void Start()
+        {
+            graph = new Vector2IntGraph<Node<Vec2Int>>(mapWidth, mapHeight);
+            graphView.Graph = graph;
+        }
+        
+        
+    }
+}
