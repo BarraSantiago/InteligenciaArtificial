0a24788280ffe99ffda0ee083e1ad4cef7ddf26f
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2024-10-28 17:56:53 -0300

    Add new states

---
+++ Assets/Scripts/StateMachine/States/SimStates/SimWalkState.cs
@@ -0,0 +1,21 @@
+﻿namespace States.SimStates
+{
+    public class SimWalkState : State
+    {
+        public override BehaviourActions GetTickBehaviour(params object[] parameters)
+        {
+            var behaviours = new BehaviourActions();
+            return behaviours;
+        }
+
+        public override BehaviourActions GetOnEnterBehaviour(params object[] parameters)
+        {
+            return default;
+        }
+
+        public override BehaviourActions GetOnExitBehaviour(params object[] parameters)
+        {
+            return default;
+        }
+    }
+}
